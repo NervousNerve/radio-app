a3f6c911eda036c9ebbf3bec0d42165caf67b14e
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-22 11:33:24 +0200

    Create user routes

--- api/index.js
+++ api/index.js
@@ -1,15 +1,28 @@
 const express = require("express");
+const session = require("express-session");
+
+const userRoutes = require("./routes/userRoutes");
 
 const port = 3001;
-
 const app = express();
 
 app.use(express.json());
 
+app.use(
+  session({
+    secret: "dd8b4e9e-537f-48e4-8a17-1b5d52eb674c",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: "auto" },
+  })
+);
+
+app.use("/api/v1/users", userRoutes);
+
 app.listen(port, (err) => {
-	if (err) {
-		console.error("Server could not start:", err);
-		return;
-	}
-	console.log("Listening on port:", port);
+  if (err) {
+    console.error("Server could not start:", err);
+    return;
+  }
+  console.log("Listening on port:", port);
 });
