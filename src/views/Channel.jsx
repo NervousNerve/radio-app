310584f27fde036431ed482eded630f6bad565c3
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-22 21:39:28 +0200

    Work on schedule display on Channel page

--- src/views/Channel.jsx
+++ src/views/Channel.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
+import parseDate from "../util/parseDate";
+
 function Channel() {
   const { id } = useParams();
   const [channel, setChannel] = useState();
@@ -18,7 +20,9 @@ function Channel() {
         `/api/v1/schedule?channel=${id}&date=2021-04-22`
       );
       const data = await response.json();
-      console.log(data);
+      for (let e of data) {
+        e.starttimeutc = parseDate(e.starttimeutc);
+      }
       setSchedule(data);
     })();
   }, [id]);
@@ -27,9 +31,11 @@ function Channel() {
 
   return (
     <div>
-      <div className="grid-row align-left bg-light">
+      <div className="grid-row justify-start align-center bg-light">
         <img src={channel.image} style={{ height: "4rem" }} alt=""></img>
-        <p className="text-bold m-0 p-1 bg-dark">{channel.name}</p>
+        <div className="grid-row align-center bg-dark px-1 height-100">
+          <p className="text-bold m-0">{channel.name}</p>
+        </div>
         <div className="grid-row mx-1 gap-1">
           <p className="text-bold">Tablå</p>
           <p>Program</p>
@@ -39,13 +45,21 @@ function Channel() {
       <div className="grid-col gap-1">
         {schedule &&
           schedule.map((episode, i) => (
-            <div key={i} className="grid-row align-left bg-light">
-              <img
-                src={episode.imageurl}
-                style={{ height: "4rem" }}
-                alt=""
-              ></img>
-              <p className="text-bold px-1">{episode.title}</p>
+            <div key={i} className="grid-col">
+              <p className="m-0">
+                {episode.starttimeutc.toLocaleString("sv-SE", {
+                  dateStyle: "short",
+                  timeStyle: "short",
+                })}
+              </p>
+              <div className="grid-row justify-start bg-light">
+                <img
+                  src={episode.imageurl}
+                  style={{ height: "4rem" }}
+                  alt=""
+                ></img>
+                <p className="text-bold px-1">{episode.title}</p>
+              </div>
             </div>
           ))}
       </div>
