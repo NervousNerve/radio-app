956923487cf7f035524d0bf8b4a0f5dea97b28ce
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-22 16:25:17 +0200

    Remove reference to deleted component

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import List from "../components/List";
-
 function Home() {
   const [channels, setChannels] = useState();
 
