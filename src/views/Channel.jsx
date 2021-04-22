daf41c168b7817738a9f80e2e8b5e3f61ceee22e
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-22 21:03:08 +0200

    Show scheduled episodes on Channel page

--- src/views/Channel.jsx
+++ src/views/Channel.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router";
 function Channel() {
   const { id } = useParams();
   const [channel, setChannel] = useState();
+  const [schedule, setSchedule] = useState();
 
   useEffect(() => {
     (async () => {
@@ -11,13 +12,43 @@ function Channel() {
       const data = await response.json();
       setChannel(data);
     })();
-  }, []);
+
+    (async () => {
+      const response = await fetch(
+        `/api/v1/schedule?channel=${id}&date=2021-04-22`
+      );
+      const data = await response.json();
+      console.log(data);
+      setSchedule(data);
+    })();
+  }, [id]);
 
   if (!channel) return null;
 
   return (
     <div>
-      <p>{channel.name}</p>
+      <div className="grid-row align-left bg-light">
+        <img src={channel.image} style={{ height: "4rem" }} alt=""></img>
+        <p className="text-bold m-0 p-1 bg-dark">{channel.name}</p>
+        <div className="grid-row mx-1 gap-1">
+          <p className="text-bold">Tablå</p>
+          <p>Program</p>
+        </div>
+      </div>
+
+      <div className="grid-col gap-1">
+        {schedule &&
+          schedule.map((episode, i) => (
+            <div key={i} className="grid-row align-left bg-light">
+              <img
+                src={episode.imageurl}
+                style={{ height: "4rem" }}
+                alt=""
+              ></img>
+              <p className="text-bold px-1">{episode.title}</p>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
