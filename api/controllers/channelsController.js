478091a408bbb05141e42d6fcc239486d23dd609
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-22 14:09:02 +0200

    Create function for fetching one channel

--- api/controllers/channelsController.js
+++ api/controllers/channelsController.js
@@ -8,4 +8,17 @@ async function getAllChannels(req, res) {
   res.send(data.channels);
 }
 
-module.exports = { getAllChannels };
+async function getChannel(req, res) {
+  try {
+    const response = await fetch(
+      `http://api.sr.se/api/v2/channels/${req.params.id}?format=json`
+    );
+    const data = await response.json();
+    res.send(data.channel);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+}
+
+module.exports = { getAllChannels, getChannel };
--- api/routes/channelsRoutes.js
+++ api/routes/channelsRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const channelsController = require("../controllers/channelsController");
 
 router.get("", channelsController.getAllChannels);
+router.get("/:id", channelsController.getChannel);
 
 module.exports = router;
