d16ab531b9ee7c6781628855917edd5008950c04
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-22 09:55:37 +0200

    Import BrowserRouter

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const { BrowserRouter } = require("react-router-dom");
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter></BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
