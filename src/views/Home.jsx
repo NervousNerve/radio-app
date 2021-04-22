86b80cc4ca4c13d47a9695402069ba57ada6608e
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-22 16:23:01 +0200

    Render list of channels on Home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import List from "../components/List";
+
 function Home() {
+  const [channels, setChannels] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("/api/v1/channels");
+      const data = await response.json();
+      setChannels(data);
+    })();
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
+
+      <div className="grid-col gap-1">
+        {channels &&
+          channels.map((channel) => (
+            <Link
+              to={`/channels/${channel.id}`}
+              key={channel.id}
+              className="grid-row align-left bg-light"
+            >
+              <img src={channel.image} style={{ height: "4rem" }}></img>
+              <p className="text-bold px-1">{channel.name}</p>
+            </Link>
+          ))}
+      </div>
     </div>
   );
 }
