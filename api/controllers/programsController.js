1a8d6dd44640cbff388eeb5a0a1d4ccaf7459e46
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-23 20:22:45 +0200

    Handle query params in programs route

--- api/controllers/programsController.js
+++ api/controllers/programsController.js
@@ -1,10 +1,22 @@
 const fetch = require("node-fetch");
 
 async function getAllPrograms(req, res) {
+  const params = ["format=json", "pagination=false"];
+  if (req.query.channel) params.unshift("channelid=" + req.query.channel);
+  if (req.query.category)
+    params.unshift("programcategoryid=" + req.query.category);
+
   try {
-    const response = await fetch(
-      "http://api.sr.se/api/v2/programs?format=json&pagination=false"
-    );
+    const url = "http://api.sr.se/api/v2/programs/index?" + params.join("&");
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      console.log("Request:", url);
+      console.log(`Response: ${response.status} ${response.statusText}`);
+      res.status(response.status).send();
+      return;
+    }
+
     const data = await response.json();
     res.send(data.programs);
   } catch (err) {
