9b586fa74b31f71cae43d649122f211ccd167a95
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-23 21:52:16 +0200

    Add new pages to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import { BrowserRouter, Route } from "react-router-dom";
 
+import ChannelContextProvider from "./contexts/ChannelContext";
+
 import Home from "./views/Home";
 import Channel from "./views/Channel";
+import Program from "./views/Program";
+import Episode from "./views/Episode";
+import Categories from "./views/Categories";
+import Category from "./views/Category";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Route exact path="/" component={Home}></Route>
-        <Route exact path="/channels/:id" component={Channel}></Route>
+        <Route path="/channels/:id">
+          <ChannelContextProvider>
+            <Channel />
+          </ChannelContextProvider>
+        </Route>
+        <Route path="/programs/:id" component={Program}></Route>
+        <Route path="/episode/:id" component={Episode}></Route>
+        <Route exact path="/categories" component={Categories}></Route>
+        <Route path="/categories/:id" component={Category}></Route>
       </BrowserRouter>
     </div>
   );
