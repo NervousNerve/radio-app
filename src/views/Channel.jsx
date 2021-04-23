69c292d07c3c21b0ee04783f0daed544b0aa7aeb
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-23 21:51:09 +0200

    Refactor Channel page

--- src/views/Channel.jsx
+++ src/views/Channel.jsx
@@ -1,70 +1,46 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useContext } from "react";
+import { Route, withRouter } from "react-router";
+import { NavLink } from "react-router-dom";
 
-import parseDate from "../util/parseDate";
+import { ChannelContext } from "../contexts/ChannelContext";
 
-function Channel() {
-  const { id } = useParams();
-  const [channel, setChannel] = useState();
-  const [schedule, setSchedule] = useState();
+import Schedule from "./channels/Schedule";
+import Programs from "./channels/Programs";
 
-  useEffect(() => {
-    (async () => {
-      const response = await fetch("/api/v1/channels/" + id);
-      const data = await response.json();
-      setChannel(data);
-    })();
-
-    (async () => {
-      const response = await fetch(
-        `/api/v1/schedule?channel=${id}&date=2021-04-22`
-      );
-      const data = await response.json();
-      for (let e of data) {
-        e.starttimeutc = parseDate(e.starttimeutc);
-      }
-      setSchedule(data);
-    })();
-  }, [id]);
+function Channel(props) {
+  const { channel } = useContext(ChannelContext);
 
   if (!channel) return null;
 
   return (
     <div>
-      <div className="grid-row justify-start align-center bg-light">
-        <img src={channel.image} style={{ height: "4rem" }} alt=""></img>
+      <div
+        className="grid-row justify-start align-center bg-light"
+        style={{ height: "4rem" }}
+      >
+        <img src={channel.image} alt="" style={{ height: "4rem" }}></img>
         <div className="grid-row align-center bg-dark px-1 height-100">
           <p className="text-bold m-0">{channel.name}</p>
         </div>
         <div className="grid-row mx-1 gap-1">
-          <p className="text-bold">Tablå</p>
-          <p>Program</p>
+          <NavLink exact to={`/channels/${channel.id}`}>
+            Tablå
+          </NavLink>
+          <NavLink exact to={`${props.match.url}/programs`}>
+            Alla program
+          </NavLink>
         </div>
       </div>
 
-      <div className="grid-col gap-1">
-        {schedule &&
-          schedule.map((episode, i) => (
-            <div key={i} className="grid-col">
-              <p className="m-0">
-                {episode.starttimeutc.toLocaleString("sv-SE", {
-                  dateStyle: "short",
-                  timeStyle: "short",
-                })}
-              </p>
-              <div className="grid-row justify-start bg-light">
-                <img
-                  src={episode.imageurl}
-                  style={{ height: "4rem" }}
-                  alt=""
-                ></img>
-                <p className="text-bold px-1">{episode.title}</p>
-              </div>
-            </div>
-          ))}
-      </div>
+      <Route exact path={`/channels/:id`}>
+        <Schedule />
+      </Route>
+
+      <Route path={`/channels/:id/programs`}>
+        <Programs />
+      </Route>
     </div>
   );
 }
 
-export default Channel;
+export default withRouter(Channel);
