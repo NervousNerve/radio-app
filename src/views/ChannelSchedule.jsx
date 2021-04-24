3213e61a679a1bee5a51dadce63ee45d16f9b787
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-24 10:22:45 +0200

    Create date selector on schedule page

--- src/views/ChannelSchedule.jsx
+++ src/views/ChannelSchedule.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router";
+import { useQueryParam } from "use-query-params";
 import List from "../components/List";
 import { ChannelContext } from "../contexts/ChannelContext";
 import parseDate from "../util/parseDate";
@@ -9,11 +10,13 @@ function Schedule() {
   const { id } = useParams();
   const [schedule, setSchedule] = useState();
   const { channel } = useContext(ChannelContext);
+  const [dateStr, setDateStr] = useQueryParam("date");
 
   useEffect(() => {
     // Fetch schedule for this channel
     (async () => {
-      const response = await fetch(`/api/v1/schedule?channel=${id}`);
+      let url = `/api/v1/schedule?channel=${id}&date=${dateStr}`;
+      const response = await fetch(url);
       const data = await response.json();
 
       // Replace date strings like "/Date(1619215200000)/" with corresponding Date objects
@@ -23,26 +26,43 @@ function Schedule() {
       }
       setSchedule(data);
     })();
-  }, [id]);
+  }, [id, dateStr]);
 
   if (!schedule) return null;
 
   return (
-    <List
-      items={schedule
-        .filter((episode) => episode.endtimeutc >= Date.now())
-        .map((episode) => {
-          return {
-            image: episode.imageurl || channel.image,
-            text: episode.title,
-            id: episode.program.id,
-            timestamp: episode.starttimeutc,
-          };
-        })}
-      clickItem={(item) => {
-        history.push("/programs/" + item.id);
-      }}
-    />
+    <div className="grid-col gap-1 p-1">
+      <div className="grid-row justify-center gap-1">
+        <label>Datum:</label>
+        <input
+          type="date"
+          defaultValue={new Date().toLocaleString("sv-SE", {
+            dateStyle: "short",
+          })}
+          onChange={(e) => {
+            setDateStr(e.target.value);
+          }}
+        ></input>
+      </div>
+
+      <List
+        items={schedule
+          .filter(
+            (episode) => episode.endtimeutc >= Date.parse(dateStr || new Date())
+          )
+          .map((episode) => {
+            return {
+              image: episode.imageurl || channel.image,
+              text: episode.title,
+              id: episode.program.id,
+              timestamp: episode.starttimeutc,
+            };
+          })}
+        clickItem={(item) => {
+          history.push("/programs/" + item.id);
+        }}
+      />
+    </div>
   );
 }
 
