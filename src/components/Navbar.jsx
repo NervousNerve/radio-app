4f98e641aacaf143e3e308bafb565cbefff256bb
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-25 21:12:46 +0200

    Create login button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,14 @@ import style from "./css/Navbar.module.css";
 
 function Navbar() {
   return (
-    <div className="bg-black">
+    <div className="bg-black color-white">
       <div className={`${style.navbar} px-1 container`}>
         <Link to="/" className={style.logo}>
           Radio<strong>Logo</strong>
         </Link>
+        <div className="grid-row justify-end">
+          <p className="link color-white">Logga in</p>
+        </div>
       </div>
     </div>
   );
