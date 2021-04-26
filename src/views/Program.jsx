8120be09ae80f8fe3bbc737ece9c1089460bee9d
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-26 09:15:09 +0200

    Refactor Program page into separate subpages

--- src/views/Program.jsx
+++ src/views/Program.jsx
@@ -1,31 +1,39 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, createContext } from "react";
 import { useParams } from "react-router";
 import { useQueryParam } from "use-query-params";
 
+import ProgramDetails from "./ProgramDetails";
+import ProgramEpisode from "./ProgramEpisode";
+
 import Headerbar from "../components/Headerbar";
-import List from "../components/List";
-import ListItem from "../components/ListItem";
 import parseDate from "../util/parseDate";
+
+export const ProgramContext = createContext();
 
 function Program() {
   const { id } = useParams();
   const [program, setProgram] = useState();
+  // All episodes of this program
   const [episodes, setEpisodes] = useState();
+  // Selected episode object
   const [episode, setEpisode] = useState();
+  // Query param with id of selected episode
   const [episodeId, setEpisodeId] = useQueryParam("episode");
 
   useEffect(() => {
+    // Load requested program
     (async () => {
       const response = await fetch("/api/v1/programs/" + id);
       const data = await response.json();
       setProgram(data);
     })();
 
+    // Load all episode of requested program
     (async () => {
       const response = await fetch("/api/v1/episodes?program=" + id);
       const data = await response.json();
 
-      // Replace date strings like "/Date(1619215200000)/" with corresponding Date objects
+      // Replace date strings with actual Date objects
       for (let e of data) {
         e.publishdateutc = parseDate(e.publishdateutc);
       }
@@ -34,17 +42,20 @@ function Program() {
     })();
   }, [id]);
 
+  // When query param "?episode" changes
   useEffect(() => {
+    // Was the param removed?
     if (!episodeId) {
       setEpisode(null);
       return;
     }
 
+    // Load requested episode
     (async () => {
       const response = await fetch("/api/v1/episodes/" + episodeId);
       const data = await response.json();
 
-      // Replace date strings like "/Date(1619215200000)/" with corresponding Date objects
+      // Replace date strings with actual Date objects
       data.publishdateutc = parseDate(data.publishdateutc);
 
       setEpisode(data);
@@ -53,8 +64,10 @@ function Program() {
 
   if (!program) return null;
 
+  const values = { program, episode, episodes, setEpisodeId };
+
   return (
-    <div>
+    <ProgramContext.Provider value={values}>
       <Headerbar>
         <div className="grid-row align-center bg-dark px-1 height-100">
           <h1 className="font-size-md">{program.name}</h1>
@@ -66,56 +79,8 @@ function Program() {
         )}
       </Headerbar>
 
-      {!episode && (
-        <div className="container p-1">
-          <img
-            src={program.programimagetemplatewide}
-            className="width-100"
-            alt=""
-          />
-          <p>{program.description}</p>
-
-          <h2>Avsnitt</h2>
-          <div className="grid-col gap-1">
-            <List>
-              {episodes &&
-                episodes.map((episode) => (
-                  <ListItem
-                    key={episode.id}
-                    thumbnail={episode.imageurl}
-                    onClick={() => {
-                      setEpisodeId(episode.id);
-                    }}
-                  >
-                    <p className="text-bold m-0 mb-05">{episode.title}</p>
-                    <p className="font-size-sm m-0 color-dark">
-                      Sändes:{" "}
-                      <span className="">
-                        {episode.publishdateutc.toLocaleString("sv-SE", {
-                          dateStyle: "short",
-                        })}
-                      </span>
-                    </p>
-                  </ListItem>
-                ))}
-            </List>
-          </div>
-        </div>
-      )}
-
-      {episode && (
-        <div className="container p-1">
-          <img src={episode.imageurltemplate} className="width-100" alt="" />
-          <p>{episode.description}</p>
-          <p className="color-dark">
-            Sändes:{" "}
-            {episode.publishdateutc.toLocaleString("sv-SE", {
-              dateStyle: "short",
-            })}
-          </p>
-        </div>
-      )}
-    </div>
+      {!episode ? <ProgramDetails /> : <ProgramEpisode />}
+    </ProgramContext.Provider>
   );
 }
 
--- src/views/ProgramEpisode.jsx
+++ src/views/ProgramEpisode.jsx
@@ -1,5 +1,22 @@
+import { useContext } from "react";
+
+import { ProgramContext } from "./Program";
+
 function ProgramEpisode() {
-  return <div></div>;
+  const { episode } = useContext(ProgramContext);
+
+  return (
+    <div className="container p-1">
+      <img src={episode.imageurltemplate} className="width-100" alt="" />
+      <p>{episode.description}</p>
+      <p className="color-dark">
+        Sändes:{" "}
+        {episode.publishdateutc.toLocaleString("sv-SE", {
+          dateStyle: "short",
+        })}
+      </p>
+    </div>
+  );
 }
 
 export default ProgramEpisode;
