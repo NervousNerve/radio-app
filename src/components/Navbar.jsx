d873ede4b39f892b829845c33f888096cd4c3470
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-26 11:32:54 +0200

    Show UserMenu in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+
+import { UserContext } from "../contexts/UserContext";
+import UserMenu from "./UserMenu";
 
 import style from "./css/Navbar.module.css";
 
 function Navbar() {
+  const [showUserMenu, setShowUserMenu] = useState(false);
+  const { user } = useContext(UserContext);
+
   return (
     <div className="bg-black color-white">
       <div className={`${style.navbar} px-1 container`}>
@@ -10,9 +17,22 @@ function Navbar() {
           Radio<strong>Logo</strong>
         </Link>
         <div className="grid-row justify-end">
-          <p className="link color-white">Logga in</p>
+          <p
+            className="link color-white font-size-lg"
+            onClick={() => {
+              setShowUserMenu(!showUserMenu);
+            }}
+          >
+            {user && <span>{user.firstName}</span>}{" "}
+            {!showUserMenu ? (
+              <i class="far fa-user"></i>
+            ) : (
+              <i class="fas fa-times"></i>
+            )}
+          </p>
         </div>
       </div>
+      {showUserMenu && <UserMenu />}
     </div>
   );
 }
