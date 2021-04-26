c410392df05e104be88f191af2b2da8d5fbcbd3c
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-26 11:32:37 +0200

    Create UserMenu component

---
+++ src/components/UserMenu.jsx
@@ -0,0 +1,89 @@
+import { useContext, useState } from "react";
+import { UserContext } from "../contexts/UserContext";
+import style from "./css/Login.module.css";
+
+function UserMenu() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [loginFailed, setLoginFailed] = useState(false);
+
+  const { user, setUser } = useContext(UserContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setLoginFailed(false);
+
+    (async () => {
+      const response = await fetch("/api/v1/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (!data.success) {
+        setLoginFailed(true);
+        return;
+      }
+
+      setEmail("");
+      setPassword("");
+      setUser(data.user);
+    })();
+  };
+
+  return (
+    <div className="container px-1 pb-1">
+      {!user && (
+        <form className="grid-col gap-1" onSubmit={handleSubmit}>
+          <div className="grid-col">
+            <label for="email" className="mb-05">
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              required
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            ></input>
+          </div>
+
+          <div className="grid-col">
+            <label for="password" className="mb-05">
+              Lösenord:
+            </label>
+            <input
+              type="password"
+              id="password"
+              required
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            ></input>
+          </div>
+
+          <button type="submit">Logga in</button>
+
+          {loginFailed && (
+            <span className="text-center color-error">
+              Inloggning misslyckades!
+            </span>
+          )}
+
+          <span className="font-size-sm text-center">Registrera användare</span>
+        </form>
+      )}
+    </div>
+  );
+}
+
+export default UserMenu;
