4678b2f70f4890eed37e2d7cbb8a9db4b9b3e537
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-27 17:19:18 +0200

    Fetch and render favorite channels and programs on User page

--- src/views/UserChannels.jsx
+++ src/views/UserChannels.jsx
@@ -1,7 +1,46 @@
+import { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import List from "../components/List";
+import ListItem from "../components/ListItem";
+import { UserContext } from "../contexts/UserContext";
+
 function UserChannels() {
+  const history = useHistory();
+  const { favoriteChannels } = useContext(UserContext);
+  const [channels, setChannels] = useState();
+
+  useEffect(() => {
+    if (!favoriteChannels) return;
+
+    (async () => {
+      try {
+        const response = await fetch("/api/v1/channels");
+        const data = await response.json();
+        setChannels(
+          data.filter((value) => favoriteChannels.includes(value.id))
+        );
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, [favoriteChannels]);
+
   return (
-    <div>
-      <h1>Mina kanaler</h1>
+    <div className="p-1 container">
+      <List>
+        {channels &&
+          channels.map((channel) => (
+            <ListItem
+              key={channel.id}
+              thumbnail={channel.image}
+              onClick={() => {
+                history.push("/channels/" + channel.id);
+              }}
+            >
+              <p className="text-bold m-0">{channel.name}</p>
+            </ListItem>
+          ))}
+      </List>
     </div>
   );
 }
--- src/views/UserPrograms.jsx
+++ src/views/UserPrograms.jsx
@@ -1,7 +1,46 @@
+import { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import List from "../components/List";
+import ListItem from "../components/ListItem";
+import { UserContext } from "../contexts/UserContext";
+
 function UserPrograms() {
+  const history = useHistory();
+  const { favoritePrograms } = useContext(UserContext);
+  const [programs, setPrograms] = useState();
+
+  useEffect(() => {
+    if (!favoritePrograms) return;
+
+    (async () => {
+      try {
+        const response = await fetch("/api/v1/programs");
+        const data = await response.json();
+        setPrograms(
+          data.filter((value) => favoritePrograms.includes(value.id))
+        );
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, [favoritePrograms]);
+
   return (
-    <div>
-      <h1>Mina program</h1>
+    <div className="p-1 container">
+      <List>
+        {programs &&
+          programs.map((program) => (
+            <ListItem
+              key={program.id}
+              thumbnail={program.programimage}
+              onClick={() => {
+                history.push("/programs/" + program.id);
+              }}
+            >
+              <p className="text-bold m-0">{program.name}</p>
+            </ListItem>
+          ))}
+      </List>
     </div>
   );
 }
