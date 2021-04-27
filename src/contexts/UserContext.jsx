5b8bd5990c079434bff62fbea07956e620a8a7d1
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-27 15:09:03 +0200

    Update UserContext to load and save favorites in database

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -4,6 +4,8 @@ export const UserContext = createContext();
 
 function UserContextProvider(props) {
   const [user, setUser] = useState();
+  const [favoriteChannels, setFavoriteChannels] = useState();
+  const [favoritePrograms, setFavoritePrograms] = useState();
 
   useEffect(() => {
     (async () => {
@@ -16,37 +18,97 @@ function UserContextProvider(props) {
     })();
   }, []);
 
-  const login = (email, password) => {
-    (async () => {
-      const response = await fetch("/api/v1/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email,
-          password,
-        }),
-      });
-      const data = await response.json();
+  const login = async (email, password) => {
+    const response = await fetch("/api/v1/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
 
-      if (!data.success) return false;
-      setUser(data.user);
-      return true;
-    })();
+    if (!data.success) return false;
+    setUser(data.user);
+    loadFavoriteChannels();
+    loadFavoritePrograms();
+    return true;
   };
 
-  const logout = () => {
-    (async () => {
-      const response = await fetch("/api/v1/users/logout");
-      const data = await response.json();
-      if (!data.success) return false;
-      setUser(null);
-      return true;
-    })();
+  const logout = async () => {
+    const response = await fetch("/api/v1/users/logout");
+    const data = await response.json();
+    if (!data.success) return false;
+    setUser(null);
+    setFavoriteChannels(null);
+    setFavoritePrograms(null);
+    return true;
   };
 
-  const values = { user, login, logout };
+  const loadFavoriteChannels = async () => {
+    try {
+      const response = await fetch("/api/v1/favorites/channels");
+      const data = await response.json();
+      setFavoriteChannels(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const loadFavoritePrograms = async () => {
+    try {
+      const response = await fetch("/api/v1/favorites/programs");
+      const data = await response.json();
+      setFavoritePrograms(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const saveFavoriteChannel = async (id) => {
+    const response = await fetch("/api/v1/favorites/channels", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id }),
+    });
+    const data = await response.json();
+    if (!data.success) {
+      console.error(data);
+      return;
+    }
+    setFavoriteChannels([...favoriteChannels, id]);
+  };
+
+  const saveFavoriteProgram = async (id) => {
+    const response = await fetch("/api/v1/favorites/programs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id }),
+    });
+    const data = await response.json();
+    if (!data.success) {
+      console.error(data);
+      return;
+    }
+    setFavoritePrograms([...favoritePrograms, id]);
+  };
+
+  const values = {
+    user,
+    login,
+    logout,
+    favoriteChannels,
+    favoritePrograms,
+    saveFavoriteChannel,
+    saveFavoriteProgram,
+  };
 
   return (
     <UserContext.Provider value={values}>{props.children}</UserContext.Provider>
