ac0112e0fbf5b8b26119adcf572f7005ebefa432
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-27 13:28:34 +0200

    Add links to user pages in user menu

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -1,4 +1,5 @@
 import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 
 import { UserContext } from "../contexts/UserContext";
 
@@ -26,70 +27,77 @@ function UserMenu() {
   };
 
   return (
-    <div className={`px-1 pb-1 container`}>
-      {!user && (
-        <form
-          className={`grid-col gap-1 ${style.userMenu}`}
-          onSubmit={handleSubmit}
-        >
-          <div className="grid-col">
-            <label htmlFor="email" className="mb-05">
-              Email:
-            </label>
-            <input
-              type="email"
-              id="email"
-              required
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            ></input>
-          </div>
+    <div className={"px-1 container"}>
+      <div className={`${style.userMenu}`}>
+        {!user && (
+          <form className={`grid-col gap-1 py-1`} onSubmit={handleSubmit}>
+            <div className="grid-col">
+              <label htmlFor="email" className="mb-05">
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                required
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              ></input>
+            </div>
 
-          <div className="grid-col">
-            <label htmlFor="password" className="mb-05">
-              Lösenord:
-            </label>
-            <input
-              type="password"
-              id="password"
-              required
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            ></input>
-          </div>
+            <div className="grid-col">
+              <label htmlFor="password" className="mb-05">
+                Lösenord:
+              </label>
+              <input
+                type="password"
+                id="password"
+                required
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              ></input>
+            </div>
 
-          <div className="grid-col gap-05">
-            <button type="submit" className="bg-light color-white">
-              Logga in
-            </button>
+            <div className="grid-col gap-05">
+              <button type="submit" className="bg-light color-white">
+                Logga in
+              </button>
 
-            {loginFailed && (
-              <span className="text-center color-error">
-                Inloggning misslyckades!
+              {loginFailed && (
+                <span className="text-center color-error">
+                  Inloggning misslyckades!
+                </span>
+              )}
+
+              <span className="font-size-sm text-center">
+                Registrera användare
               </span>
-            )}
+            </div>
+          </form>
+        )}
 
-            <span className="font-size-sm text-center">
-              Registrera användare
-            </span>
+        {user && (
+          <div className={`grid-col gap-1 py-1 justify-end`}>
+            <Link to="/user/channels" className="link color-white text-right">
+              Mina kanaler
+            </Link>
+
+            <Link to="/user/programs" className="link color-white text-right">
+              Mina program
+            </Link>
+
+            <button
+              className="bg-light color-white"
+              onClick={() => {
+                logout();
+              }}
+            >
+              Logga ut
+            </button>
           </div>
-        </form>
-      )}
-
-      {user && (
-        <div className={`${style.userMenu} grid-col`}>
-          <button
-            className="bg-light color-white"
-            onClick={() => {
-              logout();
-            }}
-          >
-            Logga ut
-          </button>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
