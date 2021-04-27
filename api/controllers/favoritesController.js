c610e40c09e6a96e1ec5d299c210303585c59664
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-04-27 15:07:05 +0200

    Create api routes for getting and posting favorite channels

--- api/controllers/favoritesController.js
+++ api/controllers/favoritesController.js
@@ -18,6 +18,21 @@ const getAllFavoritePrograms = (req, res) => {
   });
 };
 
+const getAllFavoriteChannels = (req, res) => {
+  if (!req.session.user) {
+    console.log("Unauthorized access");
+    res.status(403).json({ error: "Not logged in" });
+    return;
+  }
+
+  const query = `SELECT * FROM favoriteChannels WHERE userId = $userId`;
+  const params = { $userId: req.session.user.id };
+
+  db.all(query, params, (err, results) => {
+    res.json(results.map((row) => row.channelId));
+  });
+};
+
 const addFavoriteProgram = (req, res) => {
   if (!req.session.user) {
     console.log("Unauthorized access");
@@ -25,7 +40,7 @@ const addFavoriteProgram = (req, res) => {
     return;
   }
 
-  const programId = req.body.programId;
+  const programId = req.body.id;
   const query = `
     INSERT INTO favoritePrograms (userId, programId)
     VALUES ($userId, $programId)`;
@@ -43,7 +58,34 @@ const addFavoriteProgram = (req, res) => {
   });
 };
 
+const addFavoriteChannel = (req, res) => {
+  if (!req.session.user) {
+    console.log("Unauthorized access");
+    res.status(403).json({ error: "Not logged in" });
+    return;
+  }
+
+  const channelId = req.body.id;
+  const query = `
+    INSERT INTO favoritePrograms (userId, channelId)
+    VALUES ($userId, $channelId)`;
+  const params = { $userId: req.session.user.id, $channelId: channelId };
+
+  db.run(query, params, (err) => {
+    if (err) {
+      console.log(err);
+      if (err.errno == 19) res.status(403).json({ error: "Duplicate entry" });
+      else res.status(400).json({ error: err });
+      return;
+    }
+
+    res.json({ success: "Favorite channel added" });
+  });
+};
+
 module.exports = {
+  getAllFavoriteChannels,
   getAllFavoritePrograms,
+  addFavoriteChannel,
   addFavoriteProgram,
 };
--- api/routes/favoritesRoute.js
+++ api/routes/favoritesRoute.js
@@ -3,7 +3,9 @@ const express = require("express");
 const router = express.Router();
 const favoritesController = require("../controllers/favoritesController");
 
+router.get("/channels", favoritesController.getAllFavoriteChannels);
 router.get("/programs", favoritesController.getAllFavoritePrograms);
+router.post("/channels", favoritesController.addFavoriteChannel);
 router.post("/programs", favoritesController.addFavoriteProgram);
 
 module.exports = router;
