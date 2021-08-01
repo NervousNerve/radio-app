0209ebb8293ea6a06d55d688c7a04d39699b7846
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-08-01 06:41:00 +0200

    Add UserMenu to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
-import { useEffect, useRef, createContext } from "react";
+import { createContext, useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+
+import UserMenu from "./UserMenu";
 
 import style from "./css/Navbar.module.css";
 
@@ -7,12 +9,14 @@ export const NavbarContext = createContext();
 
 function Navbar() {
   const node = useRef();
+  const [showUserMenu, setShowUserMenu] = useState(false);
 
   useEffect(() => {
     const handleClickDocument = (e) => {
       if (node.current.contains(e.target)) {
         return;
       }
+      setShowUserMenu(false);
     };
 
     document.addEventListener("mousedown", handleClickDocument);
@@ -22,15 +26,35 @@ function Navbar() {
   }, []);
 
   return (
-    <div className="bg-black color-white" ref={node}>
-      <NavbarContext.Provider>
-        <div className={`${style.navbar} px-1 container`}>
-          <Link to="/" className={style.logo}>
-            Radio<strong>App</strong>
-          </Link>
+    <nav>
+      <NavbarContext.Provider value={{ setShowUserMenu }}>
+        <div className={`${style.wrapper} bg-black color-white`} ref={node}>
+          <div className={`${style.contents} px-1 container`}>
+            <Link to="/" className={style.logo}>
+              Radio<strong>App</strong>
+            </Link>
+
+            <div className="grid-row justify-end align-center gap-1">
+              <button
+                className={`font-size-lg text-center m-0 color-white ${style.navBtn}`}
+                onClick={() => {
+                  setShowUserMenu(!showUserMenu);
+                }}
+              >
+                {showUserMenu ? (
+                  <i className="fas fa-times" />
+                ) : (
+                  <i className="far fa-user" />
+                )}
+              </button>
+            </div>
+
+            {showUserMenu && <UserMenu />}
+          </div>
         </div>
       </NavbarContext.Provider>
-    </div>
+      <div className={style.spacer}></div>
+    </nav>
   );
 }
 
